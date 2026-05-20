dfcf94e2f881711754071a4bc29707d1cf75e380
Sok Phari <sokphari@example.com>
2026-05-20 16:38:30 +0700

    test

--- exercise.js
+++ exercise.js
@@ -1,296 +1,47 @@
- let provinces = [
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-09_17-35-00.jpg",
-            province: "Banteay Meanchey",
-            district: [
-                "Mongkol Borei",
-                "Malai",
-                "Poipet",
-                "Ou Chrov",
-                "Preah Netr Preah",
-                "Srei Snam",
-                "Thmar Puok",
-                "Svay Chek"
-            ]
-        },
-        {
-            image: "https://www.shutterstock.com/image-illustration/battambang-symbolize-statue-illustration-vector-600nw-2435050667.jpg",
-            province: "Battambang",
-            district: [
-                "Banan",
-                "Thma Koul",
-                "Batdambang",
-                "Bavel",
-                "Ek Phnom",
-                "Kamrieng",
-                "Koas Kralor",
-                "Moung Ruessei",
-                "Phnum Proek",
-                "Rottanak Mondol",
-                "Sangkae",
-                "Samlout",
-                "Sampov Loun",
-                "Srei Snam"
-            ]
-        },
-        {
-            image: "https://kampongcham.gov.kh/wp-content/uploads/2023/06/2019_05_09_KPC-LOGO.png",
-            province: "Kampong Cham",
-            district: [
-                "Batheay",
-                "Chamkar Leu",
-                "Kampong Cham (City)",
-                "Kampong Siem",
-                "Kang Meas",
-                "Koh Sotin",
-                "Prey Chhor",
-                "Srei Santhor",
-                "Stueng Trang"
-            ]
-        },
-        {
-            image: "https://asset.cambodia.gov.kh/provincial/sites/13/2019/07/cropped-kampongchhnang-logo-1.png",
-            province: "Kampong Chhnang",
-            district: [
-                "Baribour",
-                "Chol Kiri",
-                "Kampong Chhnang (City)",
-                "Kampong Leaeng",
-                "Krouch Chhmar",
-                "Rolea B'ier",
-                "Tuek Phos"
-            ]
-        },
-        {
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Kampong_Speu_Province_location_map.svg/800px-Kampong_Speu_Province_location_map.svg.png",
-            province: "Kampong Speu",
-            district: [
-                "Basedth",
-                "Chbar Mon (City)",
-                "Kong Pisey",
-                "Oudong",
-                "Phnom Sruoch",
-                "Samraong Tong",
-                "Thpong",
-                "Treng Trayoeng"
-            ]
-        },
-        {
-            image: "https://th.bing.com/th/id/R.4cb1ca54d0326fda863ceec3347236d7?rik=Oe03UFwJIDM7fA&riu=http%3a%2f%2fwww.khmerplaces.com%2fstorage%2fprovinces%2fSeptember2019%2fI1rCh9hKIQGtoECswJSB.jpg&ehk=eBJ01fTJ8PxdrkQiv7iNGEKIWHi1q1BZrpv0KE78K4U%3d&risl=&pid=ImgRaw&r=0",
-            province: "Kampot",
-            district: [
-                "Andoung Meas",
-                "Banteay Meas",
-                "Chhuk",
-                "Dang Tong",
-                "Kampong Bay (City)",
-                "Tuek Chhou"
-            ]
-        },
-        {
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpU6eiw0OFFUe-c116x_81Y1_A6VMR9Td1JQ&s",
-            province: "Kandal",
-            district: [
-                "Angk Snuol",
-                "Kandal Stueng",
-                "Khsach Kandal",
-                "Koh Thom",
-                "Leu Dek",
-                "Mukh Kampoul",
-                "Ponhea Lueu",
-                "Srei Ambel",
-                "Ta Khmau (City)"
-            ]
-        },
-        {
-            image: "https://sdmntpreastus2.oaiusercontent.com/files/00000000-1a84-51f6-bcf7-82449aed3d94/raw?se=2025-03-30T02%3A59%3A08Z&sp=r&sv=2024-08-04&sr=b&scid=03be0f98-4aa0-50cf-8380-31d7272667e9&skoid=3f3a9132-9530-48ef-96b7-fee5a811733f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-30T01%3A21%3A39Z&ske=2025-03-31T01%3A21%3A39Z&sks=b&skv=2024-08-04&sig=RE9cVLtt5YOM/fj/pzvJ2aGo%2BYihUSmNw1mB56fNn58%3D",
-            province: "Koh Kong",
-            district: [
-                "Botum Sakor",
- "Khemara Phoumin (City)",
-                "Koh Kong",
-                "Mondul Seima",
-                "Srae Ambel",
-                "Thmar Bang"
-            ]
-        },
-        {
-            image: "https://th.bing.com/th/id/OIP.-Wvi9jZknm6islCLCVePBgHaE8?rs=1&pid=ImgDetMain",
-            province: "Kratie",
-            district: [
-                "Chitr Borei",
-                "Kratie (City)",
-                "Prek Prasab",
-                "Sambour",
-                "Snuol",
-                "Stung Trang"
-            ]
-        },
-        {
-            image: "https://asset.cambodia.gov.kh/provincial/sites/18/2020/03/cropped-photo_2020-03-27_10-30-14-3.jpg",
-            province: "Mondulkiri",
-            district: [
-                "Kaoh Nheaek",
-                "Ou Reang",
-                "Pech Chenda (City)",
-                "Sen Monorom (City)"
-            ]
-        },
-        {
-            image: "https://cdn.cc-times.com/kh.cc-times.com/images/7274/large/64803540_2359398370963762_5730428280666849280_n.jpeg?1623850517",
-            province: "Oddar Meanchey",
-            district: [
-                "Anlong Veng",
-                "Banteay Ampil",
-                "Chongkal",
-                "Samraong (City)",
-                "Trapeang Prasat"
-            ]
-        },
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-14_23-25-04.jpg",
-            province: "Pailin",
-            district: [
-                "Pailin (City)",
-                "Sala Krau"
-            ]
-        },
-        {
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Phnom_Penh_location_map.svg/800px-Phnom_Penh_location_map.svg.png",
-            province: "Phnom Penh",
-            district: [
-                "Chamkar Mon",
-                "Daun Penh",
-                "Dangkao",
-                "Khan 7 Makara",
-                "Khan 8 Makara",
-                "Khan Chbar Ampov",
-                "Khan Kamboul",
-                "Khan Meanchey",
-                "Khan Por Senchey",
-                "Khan Preaek Pnov",
-                "Khan Russey Keo",
-                "Khan Sen Sok",
-                "Khan Stueng Mean Chey"
-            ]
-        },
-        {
-            image: "https://s9.kh1.co/__image/w=1200,h=630,q=100/7b/7baae39259b405de0f5cab340b2b556b66e8b522.png",
-            province: "Preah Vihear",
-            district: [
-                "Chey Sen",
-                "Chheb",
-                "Kulen",
-                "Rovieng",
-                "Sangkom Thmei",
-                "Tbaeng Meanchey (City)"
-            ]
-        },
-        {
-            image: "https://asset.cambodia.gov.kh/provincial/sites/21/2020/06/Master-Final-Logo-PPH-1024x1018.jpg",
-            province: "Pursat",
-            district: [
-                "Bakan",
-                "Kandieng",
-                "Krakor",
-                "Phnum Kravanh",
-                "Pursat (City)",
-                "Veal Veaeng"
-            ]
-        },
-        {
-            image: "https://i.pinimg.com/736x/5e/d2/07/5ed207a0be8756337d63078aaad671c6.jpg",
-            province: "Ratanakiri",
-            district: [
-                "Andoung Meas",
-                "Banlung (City)",
-                "Bar Kaev",
-                "Koun Mom",
-                "Lumphat",
-                "O Chum",
-                "O Ya Dav",
-                "Ta Veaeng",
-                "Veun Sai"
-            ]
-        },
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-09_22-14-38.jpg",
-            province: "Siem Reap",
-            district: [
-                "Angkor Chum",
-                "Angkor Thom",
-                "Banteay Srei",
-                "Chi Kreng",
-                "Kralanh",
-                "Prasat Bakong",
-                "Puok",
-                "Siem Reap (City)",
-                "Soutr Nikom",
-                "Srei Snam",
-                "Svay Leu",
-                "Varin"
-            ]
-        },
-        {
- image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOUSNdgZoHlAVoAf83GLVIWF2GryIGpY1TLw&s",
-            province: "Preah Sihanouk",
-            district: [
-                "Prey Nob",
-                "Sihanoukville (City)",
-                "Stung Hav"
-            ]
-        },
-        {
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Stung_Treng_Province_location_map.svg/800px-Stung_Treng_Province_location_map.svg.png",
-            province: "Stung Treng",
-            district: [
-                "Sesan",
-                "Siem Bouk",
-                "Siem Pang",
-                "Stung Treng (City)",
-                "Thala Barivat"
-            ]
-        },
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-14_12-09-43.jpg",
-            province: "Svay Rieng",
-            district: [
-                "Bavet (City)",
-                "Chantrea",
-                "Kampong Rou",
-                "Krom Chear",
-                "Meanchey",
-                "Romeas Haek",
-                "Svay Chrum",
-                "Svay Rieng (City)"
-            ]
-        },
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-02_18-00-22-1024x692.jpg",
-            province: "Takeo",
-            district: [
-                "Angkor Borei",
-                "Bati",
-                "Daun Keo (City)",
-                "Kiri Vong",
-                "Koh Andaet",
-                "Prey Kabbas",
-                "Samraong",
-                "Tram Kak",
-                "Treang"
-            ]
-        },
-        {
-            image: "https://asset.ams.com.kh/central/media/2021/05/photo_2021-05-14_16-00-34.jpg",
-            province: "Tboung Khmum",
-            district: [
-                "Dambae",
-                "Krouch Chhmar",
-                "Memot",
-                "Ou Reang Ov",
-                "Ponhea Kraek",
-                "Suong (City)",
-                "Tboung Khmum"
-            ]
-        }
-    ]
+l
+const imageArray = [
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png",
+    title: "Python Logo",
+    details: "The official logo for the Python programming language. It features two intertwined snakes, one blue and one yellow, forming a circular shape. Python is a high-level, interpreted, general-purpose programming language known for its readability and versatility in web development, data science, and more."
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/681px-C_Programming_Language.svg.png",
+    title: "C Programming Language Logo",
+    details: "The logo representing the C programming language. It's a simple, stylized letter 'C' often depicted in blue. C is a powerful and foundational programming language used extensively in system programming, embedded systems, and game development due to its efficiency and low-level control."
+  },
+  {
+    image: "https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png",
+    title: "JavaScript Logo",
+    details: "A widely recognized logo for JavaScript. It's a yellow badge with the stylized letters 'JS' in black. JavaScript is an essential scripting language for web development, enabling interactive and dynamic content on websites. It's also used in server-side environments (Node.js) and mobile app development."
+  },
+  {
+    image: "https://imgs.search.brave.com/qR255R_pMbMeR6A80wBdQ2ZX6j7G8XMmdnaehdEOcAo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/d2lrLmNvbS9jb250/ZW50L3VwbG9hZHMv/aW1hZ2VzLzczMV9q/YXZhLmpwZw",
+    title: "Java Logo",
+    details: "The official logo for the Java programming language, featuring a stylized coffee cup with rising steam, often in shades of blue and orange. Java is a class-based, object-oriented programming language designed to have as few implementation dependencies as possible, making it platform-independent."
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png",
+    title: "Go Logo",
+    details: "The logo for the Go programming language (also known as Golang). It's a simple and modern design featuring the word 'Go' in a clean, sans-serif blue font with stylized streaks suggesting speed and efficiency. Go is a statically typed, compiled language designed at Google for building scalable and performant software."
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/800px-PHP-logo.svg.png",
+    title: "PHP Logo",
+    details: "The logo for PHP: Hypertext Preprocessor. It's a stylized wordmark 'php' often enclosed in a rounded rectangle and typically in shades of purple and blue. PHP is a widely-used open-source scripting language especially suited for web development and can be embedded into HTML."
+  },
+  {
+    image: "https://imgs.search.brave.com/I_i66vdON1X4A3dmdmYi1BRKNQ86bOBXy_y1yrPJ6mU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9i/L2JkL0xvZ29fQ19z/aGFycC5zdmc",
+    title: "C# Logo (.NET)",
+    details: "The logo for the .NET platform, which is heavily associated with the C# programming language. It features a stylized purple square with rounded corners and a white dot in the center connected by a line. C# is a modern, object-oriented programming ."
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Tux.png/800px-Tux.png",
+    title: "Linux Logo (Tux)",
+    details: "While not strictly a programming language, Tux is the official mascot of the Linux kernel. This friendly penguin is widely recognized in the open-source community and often associated with development on Linux platforms, which are popular for various."
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/512px-.NET_Core_Logo.svg.png",
+    title: ".NET Logo (Platform)"
+  }
+]
